17961489232eec11a7c6908741f681ff2b79926c
Murali Krishna <murali1466>
2023-05-05 10:32:49 +0530

    hello

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,40 @@
-import ResponsiveTabs from "@/components/areaoffocussection/areaaoffocus";
-import Header from "@/components/header/header";
-import PolicyHeroSec from "@/components/policyhero/policyhero";
-export default function Page() {
+import { useSession, signIn, signOut } from "next-auth/react";
+import { Button, Box, Typography } from "@mui/material";
+export default function Home() {
+  const session = useSession();
+  console.log(session);
+  if (session.data === null) {
+    return (
+      <>
+        <Box sx={{ textAlign: "center" }}>
+          <Button
+            onClick={signIn}
+            sx={{
+              marginTop: "300px",
+              backgroundColor: "#000",
+            }}
+            variant="contained"
+          >
+            Login With github or google
+          </Button>
+        </Box>
+      </>
+    );
+  }
   return (
     <>
-      <Header />
-      <PolicyHeroSec />
-      <ResponsiveTabs />
+      <Box sx={{ textAlign: "center" }}>
+        <Typography variant="h6" sx={{ paddingTop: "300px" }}>
+          WELCOME
+        </Typography>
+        <Button
+          variant="contained"
+          sx={{ marginTop: "30px", backgroundColor: "#000" }}
+          onClick={signOut}
+        >
+          SIGN OUT
+        </Button>
+      </Box>
     </>
   );
 }
